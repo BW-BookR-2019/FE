a60e8cc5528668f9733ebb29a8e17419c9dd105c
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-09-23 13:33:45 -0400

    initial commit

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -8,15 +8,16 @@ import BookPage from './components/BookPage'
 function App() {
   return (
     <div className="App">
-     
+      
 
 
 
 
-     <div>
-       <Route path="/book-list" component={BookList}/>
-       <Route path="/book-list/:id" component={BookPage} />
-
+      <div>
+        <Route path="/book-list" component={BookList}/>
+        <Route path="/book-list/:id" component={BookPage} />
+        <Route path='/register' component={Register} />
+        <Route pathe='/login' component={login} />
       </div>
     </div>
   );
