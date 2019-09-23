abbb464b3008c37b2d590548bd6fca552ed25f46
allisonkydy <allisnelly@gmail.com>
2019-09-23 17:18:58 -0500

    commented out private routes

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -14,8 +14,11 @@ function App() {
     <div className='App'>
       <Link to='/book-list'>Book Selection</Link>
       <div>
-        <PrivateRoute exact path='/book-list' component={BookList} />
+        {/* <PrivateRoute exact path='/book-list' component={BookList} />
         <PrivateRoute path='/book-list/:id' component={BookPage} />
+        <PrivateRoute path='/add-review/:id' component={AddReviewForm} /> */}
+        <Route exact path='/book-list' component={BookList} />
+        <Route path='/book-list/:id' component={BookPage} />
         <Route path='/add-review/:id' component={AddReviewForm} />
         <Route path='/register' component={Register} />
         <Route path='/login' component={Login} />
