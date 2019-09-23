d6c2fe2bc05e6fff342da918ae5a516086ae652d
admin <admin@admins-MacBook-Pro.local>
2019-09-23 10:29:06 -0700

    routes added for booklist and bookpage

--- bookr/src/components/BookList.js
+++ bookr/src/components/BookList.js
@@ -1,2 +1,9 @@
 import React from 'react'
 
+function BookList (){
+
+     return(
+          <div>This is the list of books</div>
+     )
+}
+export default BookList
--- bookr/src/components/BookPage.js
+++ bookr/src/components/BookPage.js
@@ -1,1 +1,11 @@
 import React from 'react'
+
+function BookPage () {
+
+
+     return(
+          <div>This is the individual book page </div>
+     )
+}
+
+export default BookPage 
