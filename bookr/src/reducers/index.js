cf120a25832c88d6a2c6352233a36217f2f233e3
allisonkydy <allisnelly@gmail.com>
2019-09-23 12:45:29 -0500

    set up empty reducer and initial state

---
+++ bookr/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+  bookList: {},
+  isFetching: false,
+  error: null,
+  isLoggedIn: false
+}
+
+export const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default: 
+      return state;
+  }
+}
