4b54cb4a6577f9459009a10ceaab658705fa27a0
allisonkydy <allisnelly@gmail.com>
2019-09-23 12:59:10 -0500

    add reducer logic for GET actions

--- bookr/src/reducers/index.js
+++ bookr/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { GET_START, GET_SUCCESS, GET_FAILURE } from '../actions';
+
 const initialState = {
   bookList: {},
   isFetching: false,
@@ -7,6 +9,24 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case GET_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: null
+      }
+    case GET_SUCCESS:
+      return {
+        ...state,
+        bookList: action.payload,
+        isFetching: false
+      }
+    case GET_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isFetching: false,
+      }
     default: 
       return state;
   }
