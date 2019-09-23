c9ccf376efb2e92f4936770c83225f04e09c5770
allisonkydy <allisnelly@gmail.com>
2019-09-23 13:05:03 -0500

    imported reducer and Provider

--- bookr/src/index.js
+++ bookr/src/index.js
@@ -5,7 +5,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { reducer } from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
