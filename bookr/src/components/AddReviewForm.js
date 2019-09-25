d9092557d646580001b52624dbff84f1b69652ac
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-09-24 21:26:22 -0400

    added temporary axios to get book details

--- bookr/src/components/AddReviewForm.js
+++ bookr/src/components/AddReviewForm.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-// import {BookPage} from '../components/BookPage'
+import axios from 'axios'
+import ReactHtmlParser from 'react-html-parser';
 
 const AddReviewForm = (props) => {
-  return (
-    <div>
-      <h1>Leave a review for {props.info}</h1>
-      <h5>Review: (book.id)</h5>
-      <p>What did you think?</p>
-      <form>
-        <textarea name='review' id='review' cols='30' rows='10'></textarea>
-        <Link to='/book-list/:id'>
-          <button type='submit'>Submit</button>
-        </Link>
-        <Link to='/book-list/:id'>
-          <button type='button'>Cancel</button>
-        </Link>
-      </form>
-    </div>
-  );
+  let id = props.match.params.id
+
+  const [bookData, setBookData] = useState([])
+  const [bookAuthor, setBookAuthor] = useState([])
+  const [bookCover, setBookCover] = useState('')
+
+  useEffect(() => {
+    axios
+          .get(`https://www.googleapis.com/books/v1/volumes/${id}`)
+          .then(response => {
+            console.log(response.data.volumeInfo)
+            const data = response.data.volumeInfo
+            setBookData(data)
+            setBookAuthor(response.data.volumeInfo.authors)
+            setBookCover(response.data.volumeInfo.imageLinks.small || response.data.volumeInfo.imageLinks.thumbnail)
+          })
+  }, [id])
+
+  return(
+    <>
+      <h2>Leave a review for: </h2>
+      <img src={bookCover} alt="book cover"/>
+      <h3>{bookData.title}</h3>
+      {bookAuthor.map(item => (
+        <p key={item}> By {item}</p>
+      ))}
+      {/* FIELD: NAME */}
+      {/* RATING */}
+      {/* FIELD: TEXTBOX */}
+      <button>Add Review</button>
+      <button>Cancel</button>
+    </>
+  )
 };
 
 export default AddReviewForm;
