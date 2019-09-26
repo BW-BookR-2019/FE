5ff4e4048b71f21df33a91fac684e3ce37470d7a
allisonkydy <allisnelly@gmail.com>
2019-09-26 12:45:35 -0500

    changed base url

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export const axiosWithAuth = () => {
   const token = localStorage.getItem('token')
-  
+
   return axios.create({
-    baseURL: 'https://ks-starthere.herokuapp.com',
+    baseURL: 'https://realwillbrooks-bookr.herokuapp.com/',
     headers: {
       Authorization: `Bearer ${token}`
     }
